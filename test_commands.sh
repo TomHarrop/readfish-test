2cac7620135fe3e9dd986c3d9ad58e182209c7de
Tom Harrop <twharrop@gmail.com>
2020-10-16 15:26:37 +1300

    can't replay bulk fast5

--- test_commands.sh
+++ test_commands.sh
@@ -2,15 +2,20 @@
 
 set -eux
 
+# testing only! mount the reads directory, e.g.
+sshfs -o ro host:/path data/reads
+
 # open the shell
 singularity shell \
 	--writable-tmpfs \
 	--nv \
 	-B tmp/var:/var,tmp/run:/run \
-	readfish_0.0.5a.sif
+	readfish_0.0.5a_py3.7.sif
 
 # launch the gui
-nohup /opt/ont/ui/kingfisher/MinKNOW &> /var/ui.log &
+nohup bash -c \
+	'/opt/ont/minknow/bin/mk_manager_svc & 
+	/opt/ont/ui/kingfisher/MinKNOW' &> /var/ui.log &
 
 
 # launch a guppy server (to test)
@@ -21,8 +26,11 @@ nohup guppy_basecall_server   \
     --device "cuda:0" \
     &> /var/guppy_server.log &
 
+# edit /opt/ont/minknow/conf/package/sequencing/sequencing_MIN106_DNA.toml
+# /home/tom/Projects/readfish-test/data/reads/FAL06258_67bb20e2_42.fast5
+cp  \
+	sequencing_MIN106_DNA.toml \
+	/opt/ont/minknow/conf/package/sequencing/
 
-
-
-
-
+# check it
+cat /opt/ont/minknow/conf/package/sequencing/sequencing_MIN106_DNA.toml
